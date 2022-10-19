83110af075d1cc170eb4849c82c2ab6fc2e77a1d
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-19 21:15:41 +0530

    Show recipe details on particular recipe page

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,9 +1,29 @@
+import useFetch from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
+import { hostUrl } from '../../env-config';
+
+// styles
 import './Recipe.css';
 
 const Recipe = () => {
+  const { id } = useParams();
+  const url = hostUrl();
+  const { data: recipe, isPending, error } = useFetch(url + "/recipes/" + id);
+
   return (
-    <div>
-      Recipe
+    <div className="recipe">
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className='page-title'>{ recipe.title }</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
     </div>
   );
 }
