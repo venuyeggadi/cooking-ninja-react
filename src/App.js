48d0c577a6d56a3ba1d36f623301c6c3a62699a4
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-19 11:57:23 +0530

    Setup pages and routes for Home, Create, Search and Recipe pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// page components
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+
+// styles
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" >
+            <Home />
+          </Route>
+          <Route path="/create" >
+            <Create />
+          </Route>
+          <Route path="/search" >
+            <Search />
+          </Route>
+          <Route exact path="/recipes/:id" >
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/recipe/Recipe.js
@@ -0,0 +1,11 @@
+import './Recipe.css';
+
+const Recipe = () => {
+  return (
+    <div>
+      Recipe
+    </div>
+  );
+}
+ 
+export default Recipe;
