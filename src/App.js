ee433d8cdb6fc6896c1d3abbe879b008eea05d48
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-19 12:47:59 +0530

    Add basename prop for BrowserRouter

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Switch>
           <Route exact path="/" >
