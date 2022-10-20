e7e3e956fbec7a6324218f951403ce5607b8f154
Venu Yeggadi <venu.yeggadi@thoughtworks.com>
2022-10-20 21:05:55 +0530

    Add search functionality for searching recipes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 // styles
 import './Navbar.css';
+import Searchbar from './Searchbar';
 
 const Navbar = () => {
   return (
@@ -10,6 +11,7 @@ const Navbar = () => {
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
         </Link>
+        <Searchbar />
         <Link to="/create">Create Recipe</Link>
       </nav>
     </div>
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,9 +1,30 @@
+import { useLocation } from 'react-router-dom';
+import { hostUrl } from '../../env-config';
+import useFetch from '../../hooks/useFetch';
+
+// styles
 import './Search.css';
 
+// components
+import RecipeList from '../../components/RecipeList';
+
 const Search = () => {
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const url = hostUrl() + `/recipes?q=${query}`
+
+  const { data, error, isPending } = useFetch(url);
+
+  console.log(data)
+
   return (
     <div>
-      Search
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p> }
+      {data && <RecipeList recipes={data}/>}
     </div>
   );
 }
